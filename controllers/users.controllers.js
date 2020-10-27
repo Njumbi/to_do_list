b8a28c34369795e7910a9a47e6652be0af8ab705
toni <wambuinjumbi@gmail.com>
2020-10-27 17:22:45 +0300

    Added controllers to get views pages

---
+++ controllers/users.controllers.js
@@ -0,0 +1,16 @@
+exports.getListPage = (req, res, next) => {
+    res.render('todolist', {
+
+    })
+}
+exports.getRegistrationPage = (req, res, next) => {
+    res.render('registration', {
+
+    })
+}
+
+exports.getLoginPage = (req, res, next) => {
+    res.render('login', {
+
+    })
+}
